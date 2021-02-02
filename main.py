5a12d2e5f8c9a479cb46da5e8f12f4d587ace6c4
Gijs Thissen <57539005+GijsThissen@users.noreply.github.com>
2021-02-02 04:11:28 +0100

    Updated functions

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 import os
 import preprocessing as ppc
 import pandas as pd
-
+import pandas as pd
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import ColumnDataSource
+from bokeh.palettes import Spectral3
+import webbrowser
 
 if __name__ == '__main__':
-    df = pd.read_excel("dataset/dataset.xlsx")
 
-    ppc.create_quantitative_xlsx(df)
-    ppc.create_categorical_xlsx(df)
+    #df = pd.read_excel("dataset/dataset.xlsx")
 
-    os.system("bokeh serve app_dir\scatter.py app_dir\sagequantile.py app_dir --show")
+    # df = pd.read_excel("dataset/Quantitative.xlsx")
+    # quantitative_columns = df.columns.to_list()
+    # edited_quantitative_columns = quantitative_columns[1:]
+    #
+    # def Converter(cols):
+    #     first_list = []
+    #     second_list = []
+    #     for n, j in enumerate(cols):
+    #         first_list.append(cols[n])
+    #         _ = j.replace(" ","")
+    #         second_list.append(_)
+    #
+    #     result_list = dict(zip(first_list,second_list))
+    #     return result_list
+    #
+    # cols = Converter(edited_quantitative_columns)
+    #
+    # file = open('notsorted.pkl', 'rb')
+    # data = pickle.load(file)
+    # IMPORTANT: AGEQUANTILE MANUALLY EDDITED, SORT BY FIRST COLUMN AND IMPUTED 0 IN NAN
+    # data.to_excel("dataset/agequantile.xlsx")
+
+    # ppc.create_quantitative_xlsx(df)
+    # ppc.create_categorical_xlsx(df)
+
+
+    os.system("bokeh serve app_dir\scatter.py app_dir\sagequantile.py app_dir")
+
+
--- preprocessing.py
+++ preprocessing.py
@@ -7,18 +7,49 @@
 # Libraries
 import pandas as pd
 
+
 def dropuseless(df):
     """Drops the rows with minimal values"""
+    global fa
     fa = df.iloc[df["Red blood Cells"].dropna().index, :]
     return fa
+
+
+def tresholdfunction(df, treshold):
+    """Finds the treshold of the given value, a maximum of empty is inputted value"""
+    train = dropuseless(df)
+    a = train.isnull().sum() / len(train) * 100  # calculate amount empty
+    variables = train.columns
+    variable = []
+    for i in range(0, 111):
+        if a[i] <= treshold:  # if lower than the given threshold
+            variable.append(variables[i])
+    return variable
+
+
+def find_empty(df):
+    """Locates empty list of features and returns list"""
+    temp1 = tresholdfunction(df, 99)
+    temp2 = tresholdfunction(df, 100)
+    resultinglist = list(set(temp2) - set(temp1))
+
+    return resultinglist
+
 
 def drop_empty(df):
     """Drops the empty columns"""
     af = dropuseless(df)
 
-    empty_cols = [col for col in af.columns if af[col].isnull().all()]
+    empty_cols = find_empty(df)
     post_drop = af.drop(empty_cols, axis=1)
+
+    # The doctors at the hospital decided to put a string in a pH-value column.
+    # The following line removes "Não Realizado" from the column since it was causing issues
+    # "Não Realizado" means "unfulfilled" or "unrealized" and was therefore replaced with an empty value.
+    post_drop['Urine - pH'] = post_drop['Urine - pH'].replace(['Não Realizado'], '')
+
     return post_drop
+
 
 def create_quantitative_xlsx(data_frame):
     """Creates a separate file for Quantitative data"""
@@ -26,16 +57,20 @@ def create_quantitative_xlsx(data_frame):
 
     cols = df.columns  # total columns
     num_cols = df._get_numeric_data().columns
-    return_list = list(set(cols) - set(num_cols))  #return_list is the list of columns with the ones with numerical data
+    return_list = list(
+        set(cols) - set(num_cols))  # return_list is the list of columns with the ones with numerical data
 
-    exemption_list = ["Patient age quantile","Patient addmited to regular ward (1=yes, 0=no)","Patient addmited to semi-intensive unit (1=yes, 0=no)","Patient addmited to intensive care unit (1=yes, 0=no)"]
+    exemption_list = ["Patient age quantile", "Patient addmited to regular ward (1=yes, 0=no)",
+                      "Patient addmited to semi-intensive unit (1=yes, 0=no)",
+                      "Patient addmited to intensive care unit (1=yes, 0=no)"]
     # In case you consider pH and Leukocytes to be Quantitative uncomment the following 2 lines:
     # return_list.remove('Urine - pH')
     # return_list.remove('Urine - Leukocytes')
     return_list += exemption_list
 
-    numerical_set = df.drop(return_list,axis=1)
+    numerical_set = df.drop(return_list, axis=1)
     numerical_set.to_excel("dataset/Quantitative.xlsx")
+
 
 def create_categorical_xlsx(data_frame):
     """Creates a separate file for Categorical data"""
@@ -52,13 +87,12 @@ def create_categorical_xlsx(data_frame):
     # exemption_list = ['Urine - pH','Urine - Leukocytes']
     # num_cols += exemption_list
 
-    categorical_set = df.drop(num_cols,axis=1)
+    categorical_set = df.drop(num_cols, axis=1)
     categorical_set.to_excel("dataset/Categorical.xlsx")
+
 
 if __name__ == '__main__':
     af = pd.read_excel("dataset/dataset.xlsx")
 
-
     create_quantitative_xlsx(af)
     create_categorical_xlsx(af)
-
