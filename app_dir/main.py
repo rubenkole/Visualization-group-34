7172177c26546418fca4d21b73ba929cf0a20e59
Gijs Thissen <57539005+GijsThissen@users.noreply.github.com>
2021-02-03 21:38:18 +0100

    Added comments
    Comments and made them a bit faster

--- app_dir/main.py
+++ app_dir/main.py
@@ -1,4 +1,3 @@
-
 from bokeh.models import Select, FactorRange
 from bokeh.layouts import column
 from bokeh.io import curdoc
@@ -9,69 +8,94 @@ from bokeh.models import ColumnDataSource
 import pandas as pd
 from os.path import dirname, join
 
-df = pd.read_excel("dataset/Categorical.xlsx")
-cols = df.columns.to_list()
-cols = cols[1:]
+# All the options allowed in the drop down menu (deemed categorical)
+options = ['Patient age quantile', 'SARS-Cov-2 exam result', 'Ward', 'Patient addmited to regular ward (1=yes, 0=no)',
+           'Patient addmited to semi-intensive unit (1=yes, 0=no)',
+           'Patient addmited to intensive care unit (1=yes, 0=no)', 'Respiratory Syncytial Virus', 'Influenza A',
+           'Influenza B', 'Parainfluenza 1', 'CoronavirusNL63', 'Rhinovirus/Enterovirus', 'Coronavirus HKU1',
+           'Parainfluenza 3', 'Chlamydophila pneumoniae', 'Adenovirus', 'Parainfluenza 4', 'Coronavirus229E',
+           'CoronavirusOC43', 'Inf A H1N1 2009', 'Bordetella pertussis', 'Metapneumovirus', 'Parainfluenza 2',
+           'Influenza B, rapid test', 'Influenza A, rapid test', 'Strepto A', 'Urine - Esterase', 'Urine - Aspect',
+           'Urine - Hemoglobin', 'Urine - Bile pigments', 'Urine - Ketone Bodies', 'Urine - Urobilinogen',
+           'Urine - Protein', 'Urine - Leukocytes', 'Urine - Crystals', 'Urine - Hyaline cylinders',
+           'Urine - Granular cylinders', 'Urine - Yeasts', 'Urine - Color']
+
+# Load in the improved dataset
+df = pd.read_excel("dataset/improved.xlsx")
 
 
-def Converter(cols):
-    l = []
-    for n, j in enumerate(cols):
-        l.append(cols[n])
-        l.append(j)
-    outRes = dict((l[i], l[i]) if i < len(l) else (l[i], '') for i in range(len(l)))
-    return outRes
+# Counts the instances of a certain option and returns both the instances and the categories
+def counter(option):
+    """Counts the instances of a certain option and returns both the instances and the categories"""
+    # Count the values of the option and convert it to a dictionary
+    my_dict = df[str(option)].value_counts().to_dict()
 
-
-cols = Converter(cols)
-print(cols)
-source = ColumnDataSource(df)
-
-my_dict = df[str("Influenza A")].value_counts().to_dict()
-k = list(my_dict.keys())
-key = [str(_) for _ in k]
-value = list(my_dict.values())
-
-source = ColumnDataSource(data=dict(x=key, y=value))
-
-p = figure(x_range=FactorRange(factors=list(source.data['x'])))
-
-p.vbar(x="x", top="y", width=0.7, source=source)
-
-
-
-def update():
-    df = pd.read_excel("dataset/Categorical.xlsx")
-
-    my_dict = df[str(x_axis.value)].value_counts().to_dict()
+    # Get the keys from my_dict put them in a list and convert them to a string
     k = list(my_dict.keys())
-    key = [str(_) for _ in k]
+    key = [str(element) for element in k]
 
+    # Get the values from my_dict put them in a list
     value = list(my_dict.values())
 
+    return key, value
+
+
+# Create the ColumnDataSource for the plot
+source = ColumnDataSource(data=dict(x=[], y=[]))
+
+# Create the figure
+
+# Create the body of the figure. The range depends on the categories
+p = figure(title='', x_range=FactorRange(factors=list(source.data['x'])))
+
+# Create the bars
+p.vbar(x="x", top="y", width=0.7, source=source)
+
+# Label the x- and y-axis
+p.xaxis.axis_label = "Categories"
+p.yaxis.axis_label = "Patients"
+
+
+# Update function gets called when the select menu is changed
+def update():
+    """Update function gets called everytime the select menu changes"""
+
+    # Update the categories and the amount using the counter() function
+    categories, amount = counter(x_axis.value)
+
+    # Update the source dictionary to contain the current variable
     source.data = dict(
-        x=key,
-        y=value
+        x=categories,
+        y=amount
     )
+
+    # Update the x range
     p.x_range.factors = list(source.data['x'])
 
+    # Update the title
+    p.title.text = x_axis.value
 
-x_axis = Select(title="Please select a feature: ", options=sorted(cols.keys()), value="Influenza A")
 
+# Create a select menu to select what feature is to be displayed on the x-axis
+x_axis = Select(title="Please select a feature: ", options=sorted(options), value="Influenza A")
 
+# When one of the select menus is changed. Run update()
 controls = [x_axis]
+
 for control in controls:
     x_axis.on_change('value', lambda attr, old, new: update())
 
-
+# The heading is a html file can be found under app_dir/heading.html
 heading = Div(text=open(join(dirname(__file__), "heading.html")).read(), sizing_mode="stretch_width")
 
+# Update function is called to initialize the variables
 update()
 
-
+# The input column is made so it can fit on the side of the page
 inputs = column(*controls, width=320, height=1000)
 inputs.sizing_mode = "fixed"
 
+# Layout for the webpage
 layout = layout([
     [heading],
     [inputs, p],
--- app_dir/scatter.py
+++ app_dir/scatter.py
@@ -1,106 +1,115 @@
-from bokeh.models import Select
-from bokeh.layouts import column
-from bokeh.io import curdoc
-from bokeh.models.widgets import Div
-from bokeh.layouts import layout
-from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource
-from bokeh.models.tools import HoverTool
 import pandas as pd
+from bokeh.io import curdoc
+from bokeh.layouts import column, layout
+from bokeh.models import Select, ColumnDataSource
+from bokeh.models.widgets import Div
+from bokeh.plotting import figure
 from os.path import dirname, join
 
-df = pd.read_excel("dataset/Quantitative.xlsx")
-# categorical_df = pd.read_excel("dataset/Categorical.xlsx")
+# All the options allowed in the drop down menu (deemed quantitative)
+options = ['Hematocrit', 'Hemoglobin', 'Platelets', 'Mean platelet volume ', 'Red blood Cells', 'Lymphocytes',
+           'Mean corpuscular hemoglobin concentration\xa0(MCHC)', 'Leukocytes', 'Basophils',
+           'Mean corpuscular hemoglobin (MCH)', 'Eosinophils', 'Mean corpuscular volume (MCV)', 'Monocytes',
+           'Red blood cell distribution width (RDW)', 'Serum Glucose', 'Neutrophils', 'Urea',
+           'Proteina C reativa mg/dL', 'Creatinine', 'Potassium', 'Sodium', 'Alanine transaminase',
+           'Aspartate transaminase', 'Gamma-glutamyltransferase\xa0', 'Total Bilirubin', 'Direct Bilirubin',
+           'Indirect Bilirubin', 'Alkaline phosphatase', 'Ionized calcium\xa0', 'Magnesium',
+           'pCO2 (venous blood gas analysis)', 'Hb saturation (venous blood gas analysis)',
+           'Base excess (venous blood gas analysis)', 'pO2 (venous blood gas analysis)',
+           'Total CO2 (venous blood gas analysis)', 'pH (venous blood gas analysis)',
+           'HCO3 (venous blood gas analysis)', 'Rods #', 'Segmented', 'Promyelocytes',
+           'Metamyelocytes', 'Myelocytes', 'Myeloblasts', 'Urine - Density', 'Urine - Red blood cells',
+           'Relationship (Patient/Normal)', 'International normalized ratio (INR)', 'Lactic Dehydrogenase',
+           'Creatine phosphokinase\xa0(CPK)\xa0', 'Ferritin', 'Arterial Lactic Acid', 'Lipase dosage', 'Albumin',
+           'Hb saturation (arterial blood gases)', 'pCO2 (arterial blood gas analysis)',
+           'Base excess (arterial blood gas analysis)', 'pH (arterial blood gas analysis)',
+           'Total CO2 (arterial blood gas analysis)', 'HCO3 (arterial blood gas analysis)',
+           'pO2 (arterial blood gas analysis)', 'Arteiral Fio2', 'Phosphor', 'ctO2 (arterial blood gas analysis)']
 
+# Load in the improved dataset
 dataset = pd.read_excel("dataset/improved.xlsx")
+
+# Sort the dataset into two datasets: Positive and Negative
 positive_dataset = dataset[dataset['SARS-Cov-2 exam result'] == 'positive']
 negative_dataset = dataset[dataset['SARS-Cov-2 exam result'] == 'negative']
 
-quantitative_columns = df.columns.to_list()
-edited_quantitative_columns = quantitative_columns[1:]
+# Create the ColumnDataSource for Positive and Negative
+positive_source = ColumnDataSource(
+    data=dict(x=[], y=[], hematocrit=[], age_quantile=[]))
+negative_source = ColumnDataSource(
+    data=dict(x=[], y=[], hematocrit=[], age_quantile=[]))
 
+# Create figure
 
-def Converter(cols):
-    l = []
-    for n, j in enumerate(cols):
-        l.append(cols[n])
-        l.append(j)
-    outRes = dict((l[i], l[i]) if i < len(l) else (l[i], '') for i in range(len(l)))
-    return outRes
-
-
-cols = Converter(edited_quantitative_columns)
-
-# source = ColumnDataSource(data=dict(x=[], y=[],hematocrit = [],results = [],age_quantile=[]))
-
-positive_source = ColumnDataSource(data=dict(x=[], y=[], hematocrit=[], age_quantile=[]))
-negative_source = ColumnDataSource(data=dict(x=[], y=[], hematocrit=[], age_quantile=[]))
-
+# Tool tips occur when the user hovers over the datapoint
 TOOLTIPS = [
-    ("Hematocrit", "@hematocrit"),
+    ("Unit", "@hematocrit"),
     ("Age quantile", "@age_quantile")
 ]
 
+# Create the body of the figure
 p = figure(plot_height=600, plot_width=700, title="", tooltips=TOOLTIPS, toolbar_location="right",
            sizing_mode="scale_both")
-# p.circle(x="x", y="y", source=source, size=7, color="blue")
+
+# Create both the circle groups. One for Positive and one for Negative
 p.circle(x="x", y="y", source=positive_source, color="blue", legend="positive", size=6)
 p.circle(x="x", y="y", source=negative_source, color="orange", legend="negative", size=6)
+
+# Create a legend that will hide the option when clicked
 p.legend.click_policy = 'hide'
 
 
+# Update function gets called when one of the select menus is changed
 def update():
-    # global title_name
-    # df = pd.read_excel("dataset/Quantitative.xlsx")
-    # dataset = pd.read_excel("dataset/dataset.xlsx")
-    # positive_dataset = dataset[dataset['SARS-Cov-2 exam result'] == 'positive']
-    # negative_dataset = dataset[dataset['SARS-Cov-2 exam result'] == 'negative']
-
-    x_name = cols[x_axis.value]
-    y_name = cols[y_axis.value]
-
+    """Update function gets called everytime on of the select menus change"""
+    # Update the labels on the axes
     p.xaxis.axis_label = x_axis.value
     p.yaxis.axis_label = y_axis.value
 
-    # source.data = dict(
-    #     x=df[x_name],
-    #     y=df[y_name],
-    #     hematocrit = df["Hematocrit"],
-    #     results = categorical_df["SARS-Cov-2 exam result"],
-    #     age_quantile = categorical_df["Patient age quantile"]
-    # )
+    # Update the title
+    p.title.text = x_axis.value + " & " + y_axis.value
 
+    # Update the positive source dictionary to contain the current variable
     positive_source.data = dict(
-        x=positive_dataset[x_name],
-        y=positive_dataset[y_name],
-        hematocrit=positive_dataset["Hematocrit"],
+        x=positive_dataset[x_axis.value],
+        y=positive_dataset[y_axis.value],
+        hematocrit=positive_dataset["Ward"],
         age_quantile=positive_dataset["Patient age quantile"]
     )
 
+    # Update the negative source dictionary to contain the current variable
     negative_source.data = dict(
-        x=negative_dataset[x_name],
-        y=negative_dataset[y_name],
-        hematocrit=negative_dataset["Hematocrit"],
+        x=negative_dataset[x_axis.value],
+        y=negative_dataset[y_axis.value],
+        hematocrit=negative_dataset["Ward"],
         age_quantile=negative_dataset["Patient age quantile"]
     )
 
 
-x_axis = Select(title="X-axis", options=sorted(cols.keys()), value="Serum Glucose")
-y_axis = Select(title="Y-axis", options=sorted(cols.keys()), value="Hemoglobin")
+# Create two select menus. One for the x-axis and one for the y-axis
+x_axis = Select(title="X-axis", options=sorted(options), value="Serum Glucose")
+y_axis = Select(title="Y-axis", options=sorted(options), value="Hemoglobin")
 
+# When one of the select menus is changed. Run update()
 controls = [x_axis, y_axis]
-for control in controls:
-    control.on_change('value', lambda attr, old, new: update())
 
+for slider in controls:
+    slider.on_change('value', lambda attr, old, new: update())
+
+# The heading is a html file can be found under app_dir/heading.html
 heading = Div(text=open(join(dirname(__file__), "heading.html")).read(), sizing_mode="stretch_width")
 
+# Update function is called to initialize the variables
 update()
 
+# The input column is made so it can fit on the side of the page
 inputs = column(*controls, width=320, height=1000)
 inputs.sizing_mode = "fixed"
 
+# Layout for the webpage
 layout = layout([
     [heading],
     [inputs, p],
 ])
+
 curdoc().add_root(layout)
