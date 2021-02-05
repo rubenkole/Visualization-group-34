7ae074fc45c6cd229a0a35bdaff5d4ba8306a29e
Gijs Thissen <57539005+GijsThissen@users.noreply.github.com>
2021-02-05 05:01:02 +0100

    Updated main.py
    -Added proper paths for new names of files

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import os
 import webbrowser
 
 if __name__ == '__main__':
-    webbrowser.open("http://localhost:5006/main")
-    os.system("bokeh serve app_dir\scatter.py app_dir\main.py")
-
+    webbrowser.open("http://localhost:5006/quantitative")
+    os.system("bokeh serve app_dir\quantitative.py app_dir\categorical.py")
